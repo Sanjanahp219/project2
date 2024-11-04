5f116833e76ff91af83ad17e78029c3c7046004a
Sanjanahp219 <sanjanahp2020@gmail.com>
2024-11-04 22:05:30 +0530

    Describe your changes here

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Card from './Card.js'
+
+
+
+function App() {
+  return (
+    <>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import ReactImage from './Components/ReactImage.jpg'
+
+function Card(){
+  return(
+     
+    <div className="card">
+       <img className="card-img" src="" alt="Profile Picture"></img>
+       <h2>Bro Code</h2>
+       <p>I make YouTube Videos</p>
+
+    </div>
+
+
+
+  );
+
+}
+
+export default Card;
